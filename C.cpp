911d4a094a0a5adece1f06672a553dd6a0d7cab7
Bruksin <smirnov.pa@phystech.edu>
2021-11-25 14:26:10 +0300

    Fixed C.cpp

--- C.cpp
+++ C.cpp
@@ -4,120 +4,228 @@
 #include <vector>
 #include <queue>
 
-#define BIG_NUM 1000000000
+const int64_t kUndefinedDist = 1000 * 1000 * 1000 * 1LL;
+using VertexT = int32_t;
+using SquareT = int32_t;
 
-struct field {
-    int x;
-    int y;
+enum { ORIENTED = true, NOT_ORIENTED = false };
+
+struct Square {
+    VertexT x;
+    VertexT y;
 };
 
-class Graph {
+class IGraph {
 protected:
-    uint64_t vertex_degree = 0;
+    VertexT vertex_num_ = 0;
+    bool oriented_;
 
 public:
-    [[nodiscard]] uint64_t getVertexDegree() const {
-        return vertex_degree;
+    [[nodiscard]] VertexT GetVertexNum() const {
+        return vertex_num_;
     }
 
-    [[nodiscard]] virtual std::vector<uint64_t> getNeighbors(const uint64_t& v) const = 0;
+    [[nodiscard]] virtual std::vector<VertexT> GetNeighbors(const VertexT& current_vertex) const = 0;
 
-    virtual void addEdge(const uint64_t& source, const uint64_t& dest) = 0;
-
-    virtual void setFieldValue(const uint64_t& value, const int& x, const int& y) = 0;
-
-    [[nodiscard]] virtual uint64_t getValue(const int& x, const int& y) const = 0;
+    virtual void AddEdge(const VertexT& vertex_from, const VertexT& vertex_to) = 0;
 };
 
-class GraphMatrix final : public Graph {
+class GraphList final : public IGraph {
 private:
-    std::vector<std::vector<uint64_t>> matrix;
+    std::vector<std::vector<VertexT>> adjacency_list_;
 
 public:
-    explicit GraphMatrix(const uint64_t& vertex_number) {
-        for (uint64_t i = 0; i < vertex_number + 1; i++) {
-            std::vector<uint64_t> tmp(vertex_number + 1, BIG_NUM);
-            matrix.push_back(tmp);
+    explicit GraphList(const VertexT& vertex_number, bool oriented_status) {
+        vertex_num_ = vertex_number;
+        adjacency_list_.resize(vertex_number);
+        for (VertexT i = 0; i < vertex_number; i++) {
+            adjacency_list_[i] = std::vector<VertexT>();
         }
-        vertex_degree = vertex_number;
+        oriented_ = oriented_status;
     }
 
-    [[nodiscard]] std::vector<uint64_t> getNeighbors(const uint64_t& v) const override {
-        std::vector<uint64_t> answer;
-        for (uint64_t i = 0; i < getVertexDegree(); i++) {
-            if (matrix[v][i] != 0) {
+    [[nodiscard]] std::vector<VertexT> GetNeighbors(const VertexT& v) const override {
+        return adjacency_list_.at(v);
+    }
+
+    void AddEdge(const VertexT& vertex_from, const VertexT& vertex_to) override {
+        adjacency_list_[vertex_from].push_back(vertex_to);
+        if (!oriented_) {
+            adjacency_list_[vertex_to].push_back(vertex_from);
+        }
+    }
+};
+
+class GraphMatrix final : public IGraph {
+private:
+    std::vector<std::vector<VertexT>> matrix_;
+
+public:
+    explicit GraphMatrix(const VertexT& quantity) {
+        for (VertexT i = 0; i < quantity; ++i) {
+            std::vector<VertexT> tmp(quantity, 0);
+            matrix_.push_back(tmp);
+        }
+        vertex_num_ = quantity;
+    }
+
+    [[nodiscard]] std::vector<VertexT> GetNeighbors(const VertexT& current_vertex) const override {
+        std::vector<VertexT> answer;
+        for (VertexT i = 0; i < GetVertexNum(); ++i) {
+            if (matrix_[current_vertex][i] != 0) {
                 answer.push_back(i);
             }
         }
         return answer;
     }
 
-    void addEdge(const uint64_t& source, const uint64_t& dest) override {
-        if (source == dest) {
+    void AddEdge(const VertexT& vertex_from, const VertexT& vertex_to) override {
+        if (vertex_from == vertex_to) {
             return;
         }
-        matrix[source][dest] = 1;
-        matrix[dest][source] = 1;
-    }
-
-    void setFieldValue(const uint64_t& value, const int& x, const int& y) override {
-        matrix[x][y] = value;
-    }
-
-    [[nodiscard]] uint64_t getValue(const int& x, const int& y) const override {
-        return matrix[x][y];
+        matrix_[vertex_from][vertex_to] = 1;
+        if (!oriented_) {
+            matrix_[vertex_to][vertex_from] = 1;
+        }
     }
 };
 
-std::vector<field> shortestPath(Graph& graph, int start_x, int start_y, int finish_x, int finish_y) {
-    std::queue<field> queue;
-    std::vector<std::vector<field>> parent(graph.getVertexDegree() + 1, std::vector<field>(graph.getVertexDegree() + 1, {INT32_MAX, INT32_MAX}));
+struct Path {
+    std::vector<VertexT> dist;
+    std::vector<VertexT> predecessor;
 
+    explicit Path(VertexT size) {
+        predecessor.resize(size, kUndefinedDist);
+        dist.resize(size, kUndefinedDist);
+    }
+};
 
-    int possible_moves_x[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
-    int possible_moves_y[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+void BuildShortestPath(const IGraph& graph, const VertexT& source, Path& path) {
+    std::queue<VertexT> queue;
 
-    graph.setFieldValue(0, start_x, start_y);
-    std::vector<field> answer;
+    path.dist[source] = 0;
+    queue.push(source);
 
-    queue.push(field{start_x, start_y});
     while (!queue.empty()) {
-        int current_x = queue.front().x;
-        int current_y = queue.front().y;
+        VertexT current_vertex = queue.front();
         queue.pop();
-        for (int step = 0; step < 8; step++) {
-            int next_x = current_x + possible_moves_x[step];
-            int next_y = current_y + possible_moves_y[step];
-            if (next_x > 0 && next_x <= graph.getVertexDegree() && next_y > 0 && next_y <= graph.getVertexDegree() && graph.getValue(next_x, next_y) > graph.getValue(current_x, current_y) + 1) {
-                answer.push_back(field{next_x, next_y});
-                graph.setFieldValue(graph.getValue(current_x, current_y) + 1, next_x, next_y);
-                parent[next_x][next_y] = {current_x, current_y};
-                queue.push(field{next_x, next_y});
+        std::vector<VertexT> neighbors = graph.GetNeighbors(current_vertex);
+
+        for (auto& neighbor : neighbors) {
+            if (path.dist[neighbor] == kUndefinedDist) {
+                path.dist[neighbor] = path.dist[current_vertex] + 1;
+                path.predecessor[neighbor] = current_vertex;
+                queue.push(neighbor);
             }
         }
     }
+}
 
-    std::cout << graph.getValue(finish_x, finish_y) << "\n";
-    field res {finish_x, finish_y};
-    while (!(res.x == INT32_MAX && res.y == INT32_MAX)) {
-        answer.push_back(res);
-        res = parent[res.x][res.y];
+std::vector<VertexT> RestorePath(const VertexT destination, Path& path) {
+    std::vector<VertexT> shortest_path;
+    if (path.dist[destination] == kUndefinedDist) {
+        return shortest_path;
     }
-    return answer;
+
+    VertexT vertex = destination;
+    while (vertex != kUndefinedDist) {
+        shortest_path.push_back(vertex);
+        vertex = path.predecessor[vertex];
+    }
+    std::reverse(shortest_path.begin(), shortest_path.end());
+
+    return shortest_path;
+}
+
+void PrintShortestPath(const std::vector<VertexT>& path_list, VertexT size_of_field) {
+    if (path_list.empty()) {
+        std::cout << -1 << "\n";
+    } else {
+        std::cout << path_list.size() - 1 << "\n";
+        for (auto& vertex : path_list) {
+            auto vertex_x = vertex / size_of_field;
+            auto vertex_y = vertex % size_of_field;
+            std::cout << vertex_x + 1 << " " << vertex_y + 1 << "\n";
+        }
+    }
+}
+
+std::vector<VertexT> ShortestPath(const IGraph& graph, const VertexT start, const VertexT finish) {
+    Path path(graph.GetVertexNum());
+
+    BuildShortestPath(graph, start, path);
+    auto restored_path = RestorePath(finish, path);
+    return restored_path;
+}
+
+class Grid {
+    VertexT size_of_field_;
+    bool inside_field_;
+
+public:
+    explicit Grid(VertexT size) {
+        size_of_field_ = size;
+        inside_field_ = false;
+    }
+
+    VertexT ConvertXYPositionToLinear(VertexT pos_x, VertexT pos_y) const {
+        return pos_x * size_of_field_ + pos_y;
+    }
+
+    bool CheckIfInsideField(VertexT pos_x, VertexT pos_y) const {
+        if (pos_x < 0 || pos_x >= size_of_field_ || pos_y < 0 || pos_y >= size_of_field_) {
+            return false;
+        }
+        return true;
+    }
+
+    VertexT GetSize() {
+        return size_of_field_;
+    }
+};
+
+struct Move {
+    std::vector<Square> possible_moves = {{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}};
+};
+
+void SetNeighbors(IGraph& graph, Grid& grid) {
+    Move move;
+
+    for (VertexT pos_x = 0; pos_x < grid.GetSize(); pos_x++) {
+        for (VertexT pos_y = 0; pos_y < grid.GetSize(); pos_y++) {
+            for (size_t step = 0; step < move.possible_moves.size(); step++) {
+                Square potential_move = move.possible_moves[step];
+                if (grid.CheckIfInsideField(pos_x + potential_move.x, pos_y + potential_move.y)) {
+                    VertexT vertex_from = grid.ConvertXYPositionToLinear(pos_x, pos_y);
+                    VertexT vertex_to =
+                            grid.ConvertXYPositionToLinear(pos_x + potential_move.x, pos_y + potential_move.y);
+
+                    graph.AddEdge(vertex_from, vertex_to);
+                }
+            }
+        }
+    }
 }
 
 int main() {
-    int vertex, start_x, start_y, finish_x, finish_y;
+    VertexT size_of_field = 0;
+    std::cin >> size_of_field;
 
-    std::cin >> vertex >> start_x >> start_y >> finish_x >> finish_y;
+    Square start{};
+    std::cin >> start.x >> start.y;
 
-    GraphMatrix graph(vertex);
+    Square finish{};
+    std::cin >> finish.x >> finish.y;
 
-    std::vector<field> answer = shortestPath(graph, start_x, start_y, finish_x, finish_y);
+    GraphList graph(size_of_field * size_of_field, NOT_ORIENTED);
 
-    for (auto c = 0; c < graph.getValue(finish_x, finish_y) + 1; c++) {
-        std::cout << answer[answer.size() - c - 1].x << " " << answer[answer.size() - c - 1].y << "\n";
-    }
+    Grid grid(size_of_field);
+    SetNeighbors(graph, grid);
+
+    VertexT linear_start = grid.ConvertXYPositionToLinear(start.x - 1, start.y - 1);
+    VertexT linear_finish = grid.ConvertXYPositionToLinear(finish.x - 1, finish.y - 1);
+    auto shortest_path = ShortestPath(graph, linear_start, linear_finish);
+    PrintShortestPath(shortest_path, size_of_field);
 
     return 0;
 }
